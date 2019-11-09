ac83e7bd09519da79def2e476008cc24500be481
ereio <9c7e2cbf20@pm.me>
2019-11-09 17:32:27 -0500

    attempting to add netlify

--- scripts/bundle-dist.sh
+++ scripts/bundle-dist.sh
@@ -7,5 +7,5 @@ cp -R ./build/assets/* ./dist/assets
 cp ./build/*.css ./dist
 cp ./build/*.html ./dist
 cp ./build/*.ico ./dist
-cp ./build/manifest.json ./dist
-find ./build -mindepth 2 -maxdepth 5 -type d -not -empty -exec cp -r {} ./dist/assets \;
+cp ./build/manifest.json ./dist 
+find ./build -mindepth 1 -maxdepth 5 -type d -not -empty -exec cp -r {} ./dist \;
