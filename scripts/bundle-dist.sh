60783495ba6ed4832c53c72976fffeeee41b0e8e
ereio <9c7e2cbf20@pm.me>
2019-11-09 21:52:35 -0500

    one more time

--- scripts/bundle-dist.sh
+++ scripts/bundle-dist.sh
@@ -4,6 +4,7 @@ rm -r ./dist
 mkdir -p ./dist 
 cp ./build/*.css ./dist
 cp ./build/*.html ./dist
+cp ./build/*.yml ./dist
 cp ./build/*.ico ./dist
 cp ./build/manifest.json ./dist 
 find ./build -mindepth 1 -maxdepth 1 -type d -not -empty -exec cp -R {} ./dist \; 
